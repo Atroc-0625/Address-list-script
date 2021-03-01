31f17d646c9a6d73976cf8bf66cf1fb87242250c
Atroc-0625 <37469514+Atroc-0625@users.noreply.github.com>
2021-03-01 18:38:45 +0800

    Create CN_Linux.sh

---
+++ CN_Linux.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+  sudoCmd="sudo"
+else
+  sudoCmd=""
+fi
+
+
+if [[ -f /etc/redhat-release ]]; then
+  release="centos"
+  systemPackage="yum"
+elif cat /etc/issue | grep -Eqi "debian"; then
+  release="debian"
+  systemPackage="apt-get"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+  release="ubuntu"
+  systemPackage="apt-get"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+  release="centos"
+  systemPackage="yum"
+elif cat /proc/version | grep -Eqi "debian"; then
+  release="debian"
+  systemPackage="apt-get"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+  release="ubuntu"
+  systemPackage="apt-get"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+  release="centos"
+  systemPackage="yum"
+fi
+
+if [ ${systemPackage} == "yum" ]; then
+    ${sudoCmd} ${systemPackage} install wget -y -q
+else
+    ${sudoCmd} ${systemPackage} install wget -y -qq
+fi
+
+wget -N --no-check-certificate -O ./CN_Linux.text http://www.iwik.org/ipcountry/CN.cidr
+
+
+cn_filename="CN_Linux.text"
+
+sed -i 's/\(.*\)/ip route add\1 via=192.168.224.1/g' ${cn_filename}
